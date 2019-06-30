8edd82f94a3562335591c076d55157b0dec031f6
M Banks <montensia1@outlook.com>
2019-06-30 23:20:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -8,6 +8,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+  
+    @articles = Article.all 
+  
+    if @articles.empty?
+      "Wait a minute, there aren't any articles!"
+    else
+      erb :index
+      
+    end
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+   # binding.pry
+    erb :show
+  end
+  
+  
+  post '/articles' do
+   # binding.pry
+    article = Article.create(params)
+    redirect "/articles/#{article[:id]}"
+  end
+  
+  get 'article/:id/edit' do
+    erb :edit
+  end
+  
+  patch 'article/:id' do
+    @article = Article.find(params[:id])
+    binding.pry
+    @article.update(params[:id])
+    redirect "article/#{article[:id]}"
+  end
+  
 end
